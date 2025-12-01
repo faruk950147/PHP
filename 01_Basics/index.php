c6dea8b88c467cba11424a2ec567cdd34b5ef9e5
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-01 08:38:08 +0600

    I have added php

---
+++ 01_Basics/index.php
@@ -0,0 +1,21 @@
+
+
+<!-- Hypertext Preprocessor -->
+<?php
+  require_once 'main.php';
+?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Dynamic PHP</title>
+</head>
+<body>
+  <h1>Name is : <?php echo $name ?></h1>
+  <h1>Age is : <?php echo $age ?></h1>
+  <h1>Roll is : <?php echo $roll ?></h1>
+  <h1>Gpa is : <?php echo $gpa ?></h1>
+</body>
+</html>
+
---
+++ 01_Basics/main.php
@@ -0,0 +1,15 @@
+<?php
+    // printing
+    echo "Hello, World!";
+
+
+    // variables
+    $name = "Faruk Ahmed";
+    $age = 23;
+    $roll = 950147;
+    $gpa = 3.65;
+
+    // constants
+    define("SCHOOL_NAME", "ABC College");
+    define("PASS_MARK", 40);
+?>
