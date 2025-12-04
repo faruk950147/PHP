4c207f28e9b9090b52d5cadaa64d75228ab53e09
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-04 17:17:05 +0600

    I have added drf

--- 13_Projects/resume/admin/index.php
+++ 13_Projects/resume/admin/index.php
@@ -1,6 +1,3 @@
-<?php
-include "../config.php";
-echo "<h1>Admin Panel</h1>";
-echo "<a href='".$admin_url."dashboard.php'>Dashboard</a>";
-echo "<a href='".$admin_url."users.php'>Users</a>";
+<?php 
+    echo 'Hi';
 ?>
