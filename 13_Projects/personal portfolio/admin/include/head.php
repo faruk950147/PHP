c0b52e3488961e8fe5ad8a5b5ff461c0932d7027
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-08 06:40:42 +0600

    I have added php

--- 13_Projects/personal portfolio/admin/include/head.php
+++ 13_Projects/personal portfolio/admin/include/head.php
@@ -12,14 +12,11 @@
 
     <title>SB Admin 2 - Dashboard</title>
 
-    <!-- Custom fonts for this template-->
-    <link href="/admin/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet" type="text/css">
-    <link
-        href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i"
-        rel="stylesheet">
+    <link href="/PHP/13_Projects/personal%20portfolio/admin/assets/vendor/fontawesome-free/css/all.min.css" rel="stylesheet">
 
-    <!-- Custom styles for this template-->
-    <link href="/admin/assets/css/sb-admin-2.min.css" rel="stylesheet">
+    <link href="/PHP/13_Projects/personal%20portfolio/admin/assets/css/sb-admin-2.min.css" rel="stylesheet">
+
+    <link href="https://fonts.googleapis.com/css?family=Nunito:200,200i,300,300i,400,400i,600,600i,700,700i,800,800i,900,900i" rel="stylesheet">
 
     <!-- data table  -->
      <link rel="stylesheet" href="//cdn.datatables.net/2.3.5/css/dataTables.dataTables.min.css">
