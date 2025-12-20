9c1024920749741e2425ee646dff96f9e738c328
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-20 10:45:09 +0600

    I have added php

--- 01 Beginner/basic/index.php
+++ 01 Beginner/basic/index.php
@@ -6,43 +6,51 @@
     - Language construct
     - Can display one or more values
 */
-echo"Hello World";
-echo "<br>";
-echo "Hello ", "World ", "PHP";
-echo "<br>";
+echo "Hello ", "World"; // Output: Hello World
+
 /*
     print
     - Language construct
     - Can display only one value
     - Always returns 1
 */
-print "Hello World";
-echo "<br>";
+print "Hello PHP"; // Output: Hello PHP
+
 /*
     print_r()
     - Function
     - Displays variable (array/object) in readable format
     - Useful for debugging
 */
-echo print_r([1, 2, 3], true);
-echo "<br>";
+$arr = [1, 2, 3];
+print_r($arr); // Output: Array ( [0] => 1 [1] => 2 [2] => 3 )
+
 /*
     var_dump()
     - Function
     - Displays variable type, value, and size
     - Useful for detailed debugging
 */
-echo "<br>";
-echo var_dump([1, 2, 3]);
-echo "<br>";
+var_dump($arr); 
+/*
+Output:
+array(3) {
+  [0]=>
+  int(1)
+  [1]=>
+  int(2)
+  [2]=>
+  int(3)
+}
+*/
+
 /*
     var_export()
     - Function
     - Displays variable as PHP code
     - Can optionally return as string
 */
-echo var_export([1,2, 3], true);
-echo "<br>";
+var_export($arr); // Output: array ( 0 => 1, 1 => 2, 2 => 3, )
 
 /*
     die() / exit()
@@ -50,15 +58,15 @@ echo "<br>";
     - Stops script execution
     - Can optionally display a message
 */
-die("Script execution stopped");
+// die("Stopping script"); // Uncomment to stop execution
 
 /*
     sprintf()
     - Function
     - Does not directly display output
-    - Creates formatted string and returns it   
+    - Creates formatted string and returns it
 */
 $name = "Faruk";
-echo sprintf("%s %s %s", $name, "World", "PHP");
-echo "<br>";
+$str = sprintf("Hello %s!", $name);
+echo $str; // Output: Hello Faruk!
 ?>
