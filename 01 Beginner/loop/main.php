a26a26b24a6cccf7c3521628ba6d78876df882cf
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-21 09:25:40 +0600

    I have added some

--- 01 Beginner/loop/main.php
+++ 01 Beginner/loop/main.php
@@ -43,5 +43,10 @@
         }
         echo $i . "<br>";
     }
-    
+    // nested loop
+    for ($i=0; $i < 10; $i++) { 
+        for ($j=0; $j < 10; $j++) { 
+            echo $i . " " . $j . "<br>";
+        }
+    }
 ?>
