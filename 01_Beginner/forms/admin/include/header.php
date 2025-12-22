12b4a624155d132ccbfa24d6869c38ffa1246493
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-22 21:14:45 +0600

    I have added some

--- 01_Beginner/forms/admin/include/header.php
+++ 01_Beginner/forms/admin/include/header.php
@@ -1,8 +1,10 @@
-<!-- ===== Header / Navbar ===== -->
 <header id="header">
     <nav class="navbar navbar-expand-lg navbar-dark bg-success">
         <div class="container">
-            <a class="navbar-brand" href="../index.php">Student App</a>
+
+            <a class="navbar-brand" href="index.php">
+                Student App
+            </a>
 
             <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
                 <span class="navbar-toggler-icon"></span>
@@ -10,13 +12,19 @@
 
             <div class="collapse navbar-collapse" id="navbarNav">
                 <ul class="navbar-nav ms-auto">
+
                     <li class="nav-item">
-                        <a class="nav-link" href="">Add Student</a>
+                        <a class="nav-link" href="/forms/admin/create.php">
+                            Add Student
+                        </a>
                     </li>
 
                     <li class="nav-item">
-                        <a class="nav-link" href="index.php">Student List</a>
+                        <a class="nav-link" href="/forms/admin/index.php">
+                            Student List
+                        </a>
                     </li>
+
                 </ul>
             </div>
         </div>
