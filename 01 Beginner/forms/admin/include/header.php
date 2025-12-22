b3bc982722766c84c3093c3c2376b7d30d6d2601
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-22 17:50:36 +0600

    I have added some

---
+++ 01 Beginner/forms/admin/include/header.php
@@ -0,0 +1,23 @@
+<!-- ===== Header / Navbar ===== -->
+<header id="header">
+    <nav class="navbar navbar-expand-lg navbar-dark bg-success">
+        <div class="container">
+            <a class="navbar-brand" href="#">Student App</a>
+
+            <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav">
+                <span class="navbar-toggler-icon"></span>
+            </button>
+
+            <div class="collapse navbar-collapse" id="navbarNav">
+                <ul class="navbar-nav ms-auto">
+                    <li class="nav-item">
+                        <a class="nav-link active" href="#">Add Student</a>
+                    </li>
+                    <li class="nav-item">
+                        <a class="nav-link" href="#">Student List</a>
+                    </li>
+                </ul>
+            </div>
+        </div>
+    </nav>
+</header>
