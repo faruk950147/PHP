1629dcad313afc6bab869b5c3646b8e607f35a4a
faruk950147 <mdomarfarukahmedcse950147@gmail.com>
2025-12-24 13:46:28 +0600

    I have added some

---
+++ 02_Intermediate/date/index.php
@@ -0,0 +1,9 @@
+<?php
+echo "Today is " . date("Y/m/d") . "<br>";
+echo "Today is " . date("Y.m.d") . "<br>";
+echo "Today is " . date("Y-m-d") . "<br>";
+echo "Today is " . date("l") . "<br>";
+echo date('l, F j, Y');
+echo '<br>';
+echo date("Y");
+?>
